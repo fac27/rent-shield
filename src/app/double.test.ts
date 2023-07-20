70e6d458f4dc9bf4305212829f190b0bdcd7e415
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-07-20 16:29:36 +0100

    cleaned test files

--- src/app/double.test.ts
+++
@@ -1,7 +0,0 @@
-import double from './double';
-
-describe('double tests', () => {
-  test('double returns double of the argument given', () => {
-    expect(double(2)).toBe(4);
-  });
-});
--- src/app/double.ts
+++
@@ -1,3 +0,0 @@
-export default function double(x:number):number{
-  return x * 2
-}
