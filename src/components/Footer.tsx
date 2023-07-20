b77cf908e94ce93e7c2fa24c94e645fd1a652252
hanleymark <home@markhanley.co.uk>
2023-07-20 12:17:56 +0100

    Components created

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { FC } from 'react'
+import { Dropdown, Navbar } from 'flowbite-react';
+
+const Header = ():FC => {
+    return (
+        <div>
+            <h1>Rent shield</h1>
+        </div>
+    )
+}
+
+export default Header
