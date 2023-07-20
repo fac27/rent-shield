1b14ec31ce1cb88ac12842afa064a6ab2328d3a6
FomasTreeman <tom@team-freeman.com>
2023-07-20 17:12:01 +0100

    lint lots off isuses

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  plugins: ['jsx-a11y', 'react', 'react-hooks', 'import', '@next/next'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
