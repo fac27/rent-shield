76d61df77a25324ea2a42e9826a6fc698d9461de
FomasTreeman <tom@team-freeman.com>
2023-07-21 17:01:53 +0100

    tests work

--- cypress/e2e/carousel.cy.ts
+++ cypress/e2e/carousel.cy.ts
@@ -3,17 +3,22 @@ describe('carousel stress tests', () => {
     cy.visit('/');
     cy.get('[alt="image-1"]').should('be.visible');
     cy.get('#right-arrow').click();
+    cy.wait(100);
     cy.get('[alt="image-2"]').should('be.visible');
     cy.get('#right-arrow').click();
+    cy.wait(100);
     cy.get('[alt="image-1"]').should('be.visible');
     cy.get('#left-arrow').click();
+    cy.wait(100);
     cy.get('[alt="image-2"]').should('be.visible');
   });
   it('should change the image when the dots are clicked', () => {
     cy.visit('/');
     cy.get('[alt="image-1"]').should('be.visible');
+    cy.wait(100);
     cy.get('#slide-2').click();
     cy.get('[alt="image-2"]').should('be.visible');
+    cy.wait(100);
     cy.get('#slide-1').click();
     cy.get('[alt="image-1"]').should('be.visible');
   });
