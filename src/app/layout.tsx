3489f45e8e666dc9ce5656f419e70b6f34bd81d0
Thomas Freeman <44851616+FomasTreeman@users.noreply.github.com>
2023-07-21 10:49:40 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import type {Metadata} from 'next';
+import type { Metadata } from 'next';
 import { Quicksand } from 'next/font/google';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
