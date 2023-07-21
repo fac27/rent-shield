1df1d89cf28eaa593a1d9f111713e1ea32d4152c
FomasTreeman <tom@team-freeman.com>
2023-07-21 12:31:43 +0100

    initial property component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import SearchBar from "@/components/SearchBar"
+import SearchBar from '@/components/SearchBar';
+import Property from '../components/Property';
 
 export default function Home() {
   return (
     <>
-    <h1>This is the home page</h1>
-    <SearchBar/>
+      <h1>This is the home page</h1>
+      <SearchBar />
+      <Property />
     </>
-  )
+  );
 }
