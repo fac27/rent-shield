b248d34d0c2dbf2866e43f46f8feab546e46c6f8
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-07-21 15:19:50 +0100

    svg attributes adjusted

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -25,9 +25,9 @@ const SearchBar: FC = (): ReactElement => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
             />
           </svg>
