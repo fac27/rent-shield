8ecd470e73e809b56bcaf68946bb7912408dd392
FomasTreeman <tom@team-freeman.com>
2023-07-21 13:52:08 +0100

    carousle

---
+++ src/components/Carousel.tsx
@@ -0,0 +1,3 @@
+export default function Carousel() {
+  return <h1>i guess ill do it myself </h1>;
+}
--- src/components/Property.tsx
+++ src/components/Property.tsx
@@ -9,149 +9,71 @@ import heart from '../../public/heart.svg';
 import fullHeart from '../../public/full-heart.svg';
 import transport from '../../public/transport.svg';
 import map from '../../public/images/map-test.png';
+import arrow from '../../public/arrow.svg';
 import { useState } from 'react';
-import { Carousel } from 'flowbite-react';
+// import { Carousel } from 'flowbite-react';
 
 export default function Property() {
   const [liked, setLiked] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  //    use props
+  //   paths
+  const images = ['/images/interior-1.jpeg', '/images/interior-3.jpeg'];
 
   return (
     <div className="flex flex-col">
-      {/* image */}
-      {/* <Image
-        src="/images/interior-3.jpeg"
-        alt="interior-3"
-        width={1000}
-        height={1000}
-      /> */}
-      <div
-        id="default-carousel"
-        className="relative w-full"
-        data-carousel="slide"
-      >
-        <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
+      {/* images */}
+      {/* animate */}
+      <section className="relative h-96">
+        <div className="flex justify-center">
+          {images.map((image, i) => (
             <Image
-              src="/images/interior-1.jpeg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={1000}
-              height={1000}
+              key={crypto.randomUUID()}
+              src={image}
+              alt="image-1"
+              layout="fill"
+              objectFit="contain"
+              style={currentSlide === i ? {} : { display: 'none' }}
             />
-          </div>
-          <div className="hidden duration-700 ease-in-out" data-carousel-item>
-            <Image
-              src="/images/interior-.jpeg"
-              className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              alt="..."
-              width={1000}
-              height={1000}
-            />
-          </div>
+          ))}
         </div>
+        <button
+          className="absolute inset-y-0 left-0 h-96 mx-4 rotate-180"
+          onClick={() =>
+            currentSlide !== 0 ? setCurrentSlide(currentSlide - 1) : ''
+          }
+        >
+          <Image src={arrow} alt="arrow-left" width={40} height={40} />
+        </button>
+        <button
+          className="absolute inset-y-0 right-0 h-96 mx-4"
+          onClick={() =>
+            currentSlide !== images.length - 1
+              ? setCurrentSlide(currentSlide + 1)
+              : ''
+          }
+        >
+          <Image src={arrow} alt="arrow-right" width={40} height={40} />
+        </button>
         <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="true"
-            aria-label="Slide 1"
-            data-carousel-slide-to="0"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 2"
-            data-carousel-slide-to="1"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 3"
-            data-carousel-slide-to="2"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 4"
-            data-carousel-slide-to="3"
-          ></button>
-          <button
-            type="button"
-            className="w-3 h-3 rounded-full"
-            aria-current="false"
-            aria-label="Slide 5"
-            data-carousel-slide-to="4"
-          ></button>
+          {images.map((_, index) => (
+            <button
+              key={crypto.randomUUID()}
+              className={`w-3 h-3 rounded-full ${
+                currentSlide === index ? 'bg-white' : 'bg-gray-300'
+              }`}
+              onClick={() => {
+                setCurrentSlide(index);
+              }}
+              aria-current="true"
+              aria-label="Slide 1"
+            ></button>
+          ))}
         </div>
-        <button
-          type="button"
-          className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M5 1 1 5l4 4"
-              />
-            </svg>
-            <span className="sr-only">Previous</span>
-          </span>
-        </button>
-        <button
-          type="button"
-          className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-next
-        >
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg
-              className="w-4 h-4 text-white dark:text-gray-800"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 6 10"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 9 4-4-4-4"
-              />
-            </svg>
-            <span className="sr-only">Next</span>
-          </span>
-        </button>
-      </div>
-      {/* <Carousel slideInterval={5000} className="h-1/2">
-        <Image
-          src="/images/interior-1.jpeg"
-          alt="interior-1"
-          width={500}
-          height={500}
-        />
-        <Image
-          src="/images/interior-3.jpeg"
-          alt="interior-3"
-          width={500}
-          height={500}
-        />
-      </Carousel> */}
+      </section>
 
       {/* details */}
-      <section className="flex justify-evenly bg-[#011627] py-5">
+      <section className="flex justify-evenly bg-[#202A37] py-5">
         <span className="flex gap-2 text-xl">
           <Image src={price} alt="price" width={20} height={20} />
           <p className="text-slate-200"> $1,500/Month</p>
@@ -187,8 +109,8 @@ export default function Property() {
       </p>
 
       {/* location info */}
-      <section className="flex justify-around">
-        <b className="flex  items-center gap-2 text-xl">
+      <section className="flex justify-around lg:justify-center lg:gap-60">
+        <b className="flex  items-center gap-2 text-sm">
           <Image
             src={location}
             alt="location"
@@ -198,7 +120,7 @@ export default function Property() {
           />
           2 mi from desired location
         </b>
-        <b className="flex items-center gap-2 text-xl">
+        <b className="flex items-center gap-2 text-sm">
           <Image src={transport} alt="transport" width={20} height={20} />
           Piccadilly Circus
         </b>
