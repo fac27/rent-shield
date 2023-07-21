40ef8a4e60c8ab310fd0be257b815783af9382d3
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-07-21 12:48:48 +0100

    search bar on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import SearchBar from "@/components/SearchBar"
+
 export default function Home() {
   return (
     <>
-    <h1>This is the home page</h1></>
+    <h1>This is the home page</h1>
+    <SearchBar/>
+    </>
   )
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,53 @@
+import { ReactElement, FC } from 'react';
+import Link from 'next/link';
+
+const SearchBar: FC = (): ReactElement => {
+  return (
+    <form>
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <Link
+          href="/searchpreferences"
+          passHref={true}
+          className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <svg
+            className="w-6 h-6 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75"
+            />
+          </svg>
+        </Link>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search Postcode or Area"
+          required
+        />
+        <button
+          type="submit"
+          className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Search
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
