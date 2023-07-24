e3a2bcc22bacc5320fec9b8e88d73734f7e99f33
cameochoquer <cjchoquer@gmail.com>
2023-07-24 17:22:31 +0100

    google maps autocompelete passes

---
+++ cypress/e2e/googlemap.cy.ts
@@ -0,0 +1,27 @@
+describe('Google Maps API', ()=>{
+    beforeEach(()=>{
+        cy.intercept('GET', '**/autocomplete/**', (req)=>{
+            if(req.url.includes('london')){
+                req.reply([
+                    {
+                        description:'London, UK',
+                        place_id: 'some_place_id'
+                    }
+                ])
+            }
+        })
+    })
+
+    it('correctly locates center', ()=>{
+        cy.visit('/')
+        cy.get('#default-search')
+        .should('be.visible')
+        .type('london')
+        .wait(2000)
+        .type('{downarrow}')
+        cy.get('body').click(0,0)
+        cy.get('#default-search')
+        .invoke('val')
+        .should('equal', 'London, UK')
+    })
+})
