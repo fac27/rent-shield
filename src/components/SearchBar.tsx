19c1a429b7790f997e2cdc11084c50952bf2c62c
cameochoquer <cjchoquer@gmail.com>
2023-07-25 14:05:54 +0100

    fixing router

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { ReactElement, FC, useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import { initializeSearch } from 'utils/mapHelper';
