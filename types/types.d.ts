132a635c750183fa29d2ff29d720150c191c8783
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-07-25 11:08:19 +0100

    adjusted PropertyType

--- types/types.d.ts
+++ types/types.d.ts
@@ -9,25 +9,27 @@ interface IMapProps {
 }
 
 export type PropertyType = {
-  address1: string;
-  address2: string | null;
-  attributes: Json | null;
-  bathrooms: number;
-  bedrooms: number;
-  city: string;
-  county: string;
-  created_at: string;
-  description: string;
-  id: number;
-  latitude: string;
-  longitude: string;
-  postcode: string;
-  rent: number;
-  status_id: number;
-  type_id: number;
-  user_id: string;
-  images: string[];
-  favourited: boolean;
+  listing: {
+    address1: string;
+    address2: string | null;
+    attributes: Json | null;
+    bathrooms: number;
+    bedrooms: number;
+    city: string;
+    county: string;
+    created_at: string;
+    description: string;
+    id: number;
+    latitude: string;
+    longitude: string;
+    postcode: string;
+    rent: number;
+    status_id: number;
+    type_id: number;
+    user_id: string;
+    images: string[];
+    favourited: boolean;
+  };
 };
 
 export interface ContainerProps {
