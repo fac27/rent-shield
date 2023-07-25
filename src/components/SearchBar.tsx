53d5f24d9ca9ba4e12601771b7e5c15c9557a0c9
FomasTreeman <tom@team-freeman.com>
2023-07-25 15:11:56 +0100

    navigation

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { ReactElement, FC, useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import Image from 'next/image';
 import { initializeSearch } from 'utils/mapHelper';
