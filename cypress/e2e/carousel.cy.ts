3c09a2d02d220c88735d7c7b0d37c97a9dd1a0c3
hanleymark <home@markhanley.co.uk>
2023-07-26 11:15:34 +0100

    Redirect home route carousel E2E test to listings

--- cypress/e2e/carousel.cy.ts
+++ cypress/e2e/carousel.cy.ts
@@ -1,25 +1,25 @@
 describe('carousel stress tests', () => {
-  // it('should change the image when arrows are clicked', () => {
-  //   cy.visit('/');
-  //   cy.get('[alt="image-1"]').should('be.visible');
-  //   cy.get('#right-arrow').click();
-  //   cy.wait(2000);
-  //   cy.get('[alt="image-2"]').should('be.visible');
-  //   cy.get('#right-arrow').click();
-  //   cy.wait(2000);
-  //   cy.get('[alt="image-1"]').should('be.visible');
-  //   cy.get('#left-arrow').click();
-  //   cy.wait(2000);
-  //   cy.get('[alt="image-2"]').should('be.visible');
-  // });
-  // it('should change the image when the dots are clicked', () => {
-  //   cy.visit('/');
-  //   cy.get('[alt="image-1"]').should('be.visible');
-  //   cy.wait(2000);
-  //   cy.get('#slide-2').click();
-  //   cy.get('[alt="image-2"]').should('be.visible');
-  //   cy.wait(2000);
-  //   cy.get('#slide-1').click();
-  //   cy.get('[alt="image-1"]').should('be.visible');
-  // });
+  it('should change the image when arrows are clicked', () => {
+    cy.visit('/listings');
+    cy.get('[alt="image-1"]').should('be.visible');
+    cy.get('#right-arrow').click();
+    cy.wait(2000);
+    cy.get('[alt="image-2"]').should('be.visible');
+    cy.get('#right-arrow').click();
+    cy.wait(2000);
+    cy.get('[alt="image-1"]').should('be.visible');
+    cy.get('#left-arrow').click();
+    cy.wait(2000);
+    cy.get('[alt="image-2"]').should('be.visible');
+  });
+  it('should change the image when the dots are clicked', () => {
+    cy.visit('/listings');
+    cy.get('[alt="image-1"]').should('be.visible');
+    cy.wait(2000);
+    cy.get('#slide-2').click();
+    cy.get('[alt="image-2"]').should('be.visible');
+    cy.wait(2000);
+    cy.get('#slide-1').click();
+    cy.get('[alt="image-1"]').should('be.visible');
+  });
 });
