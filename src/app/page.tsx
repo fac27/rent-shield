bd2773e4d75a8a77acda41118fd82759c53164e2
cameochoquer <cjchoquer@gmail.com>
2023-07-26 15:24:39 +0100

    adding in routes to the burger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import SearchBar from 'components/SearchBar';
-import Link from 'next/link';
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Link href={'/listings'}>View listings</Link>
       <div className="bg-slate-900 flex h-screen justify-center items-center w-full">
         <SearchBar/>
       </div>
