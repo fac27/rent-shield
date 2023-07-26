0a35ef01e6f9b4bf103bf374bae9df251f5c6462
cameochoquer <cjchoquer@gmail.com>
2023-07-26 16:59:34 +0100

    adding nextauth templates

--- src/app/log-in/components/LogInForm.tsx
+++ src/app/log-in/components/LogInForm.tsx
@@ -2,8 +2,29 @@
 
 import { Button, Card, Label, TextInput } from 'flowbite-react';
 import Link from 'next/link';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import type { Database } from '../../../../types/supabase';
 
 export default function SignInForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+  const supabase = createClientComponentClient<Database>()
+
+
+
+  const handleLogin = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+    router.push('/')
+  }
+
+
+
   return (
     <main className="fixed flex h-full w-screen pb-20">
       <Card className="w-8/12 p-4 m-auto">
@@ -18,18 +39,21 @@ export default function SignInForm() {
               required
               shadow
               type="email"
+              name='email'
+              onChange={(e)=>setEmail(e.target.value)}
+              value={email}
             />
           </div>
           <div>
             <div className="mb-2 block">
               <Label htmlFor="password2" value="Your password" />
             </div>
-            <TextInput id="password2" required shadow type="password" />
+            <TextInput name='password' value={password} onChange={(e)=> setPassword(e.target.value)} id="password2" required shadow type="password" />
           </div>
-          <Button className="self-end" color="purple">
+          <Button className="self-end" color="purple" >
             <Link href="/sign-up"> Don&apos;t have an account yet? </Link>
           </Button>
-          <Button type="submit">Log In</Button>
+          <Button type="submit" onClick={handleLogin}>Log In</Button>
         </form>
       </Card>
     </main>
--- src/app/sign-up/components/SignUpForm.tsx
+++ src/app/sign-up/components/SignUpForm.tsx
@@ -2,8 +2,26 @@
 
 import { Button, Card, Label, TextInput } from 'flowbite-react';
 import Link from 'next/link';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import type { Database } from '../../../../types/supabase';
 
 export default function SignUpForm() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const router = useRouter()
+  const supabase = createClientComponentClient<Database>()
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    })
+    router.push('/')
+  }
   return (
     <main className="fixed flex h-full w-screen pb-20">
       <Card className="w-8/12 p-4 m-auto">
@@ -18,24 +36,27 @@ export default function SignUpForm() {
               required
               shadow
               type="email"
+              onChange={(e)=> setEmail(e.target.value)}
+              value={email}
+              name='email'
             />
           </div>
           <div>
             <div className="mb-2 block">
               <Label htmlFor="password2" value="Your password" />
             </div>
-            <TextInput id="password2" required shadow type="password" />
+            <TextInput id="password2" required shadow type="password" name="password" onChange={(e)=> setPassword(e.target.value)}/>
           </div>
-          <div>
+          {/* <div>
             <div className="mb-2 block">
               <Label htmlFor="repeat-password" value="Repeat password" />
             </div>
             <TextInput id="repeat-password" required shadow type="password" />
-          </div>
+          </div> */}
           <Button className="self-end" color="purple">
             <Link href="/log-in"> Already have an account? </Link>
           </Button>
-          <Button type="submit">Register new account</Button>
+          <Button onClick={handleSignUp} type="submit">Register new account</Button>
         </form>
       </Card>
     </main>
