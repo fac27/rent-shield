644f3fec250e68f61262aa7bfeef968371ea9607
FomasTreeman <tom@team-freeman.com>
2023-07-26 13:48:46 +0100

    cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://127.0.0.1:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
   },
 });
--- src/components/ListingsContainter.tsx
+++ src/components/ListingsContainter.tsx
@@ -3,12 +3,16 @@ import { ReactElement, FC } from 'react';
 import Property from './Property';
 import { ContainerProps } from '../../types/types';
 
-const ListingsContainer: FC<ContainerProps> = ({listings}) => {
+const ListingsContainer: FC<ContainerProps> = ({ listings }) => {
   // console.log(listings)
   return (
     <div>
-      {listings.map(listing=> (
-        <Property listing={listing} key={listing.id} />
+      {listings.map((listing, index) => (
+        <Property
+          data-id={`listing-${index + 1}`}
+          listing={listing}
+          key={listing.id}
+        />
       ))}
     </div>
   );
