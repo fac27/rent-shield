998c789d4cbee617db8229b29721033e1ca7a409
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-07-28 14:58:48 +0100

    added route test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,9 +4,18 @@ describe('/ page spec', () => {
   });
 });
 
-describe('/listings page spec', ()=> {
-  it('loads listings available', ()=>{
+describe('/listings page spec', () => {
+  it('loads listings available', () => {
     cy.visit('http://localhost:3000/listings');
     cy.get('.test-class-property').should('be.visible');
-  })
-})
+  });
+});
+
+describe('/search-preferences spc', () => {
+  it('loads the SearchPreferenceForm component on the page', () => {
+    cy.visit('http://localhost:3000/search-preferences?locations=hackney');
+    cy.get('[data-cy="SearchPreferencesForm"]').should('be.visible');
+    cy.get('button').click
+    cy.url().should('include', '&billsIncluded=');
+  });
+});
