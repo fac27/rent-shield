86a3349ebc30a74d638e76c55fc15a9d1233be96
FomasTreeman <tom@team-freeman.com>
2023-07-28 11:07:47 +0100

    container misspelling

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,7 +1,15 @@
 import ListingsContainer from '../../components/ListingsContainter';
 import { getAllProperties } from 'lib/models';
+import { isWithin } from 'utils/locationHelper';
 
 export default async function SearchResults() {
   const listings = await getAllProperties();
-  return <ListingsContainer listings={listings} />;
+  const filteredListings = listings.filter((listing) =>
+    isWithin(
+      { lat: 51.507351, lng: -0.127758 },
+      { lat: listing.latitude, lng: listing.longitude },
+      20,
+    ),
+  );
+  return <ListingsContainer listings={filteredListings} />;
 }
--- src/utils/locationHelper.ts
+++ src/utils/locationHelper.ts
@@ -1,36 +1,36 @@
-function getDistanceBetweenLocationPair(
-    {
-        latitude: lat1,
-        longitude: lon1
-    }: { latitude: number; longitude: number },
-    {
-        latitude: lat2,
-        longitude: lon2
-    }: { latitude: number; longitude: number
-    }
-)
-{
+import type { ILocation } from '../../types/types';
 
-// Convert from degrees to radians
-lon1 =  lon1 * Math.PI / 180;
-lon2 = lon2 * Math.PI / 180;
-lat1 = lat1 * Math.PI / 180;
-lat2 = lat2 * Math.PI / 180;
-
-// Haversine formula
-const dlon = lon2 - lon1;
-const dlat = lat2 - lat1;
-const a = Math.pow(Math.sin(dlat / 2), 2)
-+ Math.cos(lat1) * Math.cos(lat2)
-* Math.pow(Math.sin(dlon / 2),2);
-
-const c = 2 * Math.asin(Math.sqrt(a));
-
-// Radius of earth in km
-const radius = 6371;
-
-// calculate the result in km
-return(c * radius);
+export function isWithin(
+  origin: ILocation,
+  location: ILocation,
+  range: number,
+) {
+  const distance = getDistanceBetweenLocationPair(origin, location);
+  return distance <= range;
 }
 
-export {getDistanceBetweenLocationPair}
+function getDistanceBetweenLocationPair(
+  { lat: lat1, lng: lng1 }: { lat: number; lng: number },
+  { lat: lat2, lng: lng2 }: { lat: number; lng: number },
+) {
+  // Convert from degrees to radians
+  lng1 = (lng1 * Math.PI) / 180;
+  lng2 = (lng2 * Math.PI) / 180;
+  lat1 = (lat1 * Math.PI) / 180;
+  lat2 = (lat2 * Math.PI) / 180;
+
+  // Haversine formula
+  const dlon = lng2 - lng1;
+  const dlat = lat2 - lat1;
+  const a =
+    Math.pow(Math.sin(dlat / 2), 2) +
+    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+  const c = 2 * Math.asin(Math.sqrt(a));
+
+  // Radius of earth in km
+  const radius = 6371;
+
+  // calculate the result in km
+  return c * radius;
+}
