9fa6dd0a29d0ee69a443e47652d5295b12a7b8c3
FomasTreeman <tom@team-freeman.com>
2023-07-28 12:46:55 +0100

    encoding dev dep for error

--- src/app/listings/page.tsx
+++ src/app/listings/page.tsx
@@ -1,15 +1,7 @@
-import ListingsContainer from '../../components/ListingsContainter';
+import ListingsContainer from '../../components/ListingsContainer';
 import { getAllProperties } from 'lib/models';
-import { isWithin } from 'utils/locationHelper';
 
 export default async function SearchResults() {
   const listings = await getAllProperties();
-  const filteredListings = listings.filter((listing) =>
-    isWithin(
-      { lat: 51.507351, lng: -0.127758 },
-      { lat: listing.latitude, lng: listing.longitude },
-      20,
-    ),
-  );
-  return <ListingsContainer listings={filteredListings} />;
+  return <ListingsContainer listings={listings} />;
 }
--- src/components/ListingsContainer.tsx
+++ src/components/ListingsContainer.tsx
@@ -1,21 +1,41 @@
-import { FC } from 'react';
+'use client';
 
 import Property from './Property';
 import { ContainerProps } from '../../types/types';
+import { isWithin } from 'utils/locationHelper';
+import { useSearchParams } from 'next/navigation';
+import { memo } from 'react';
 
-const ListingsContainer: FC<ContainerProps> = ({ listings }) => {
-  // console.log(listings)
+const ListingsContainer = ({ listings }: ContainerProps) => {
+  const searchParams = useSearchParams();
+  let filteredListings = listings;
+
+  const proximity = searchParams.get('proximity');
+  if (proximity) {
+    // lat lng and radius should always be present if proximity is present
+    const { lat, lng, radius } = Object.fromEntries(
+      new URLSearchParams(proximity),
+    );
+
+    filteredListings = listings.filter((listing) =>
+      isWithin(
+        { lat: parseInt(lat), lng: parseInt(lng) },
+        { lat: listing.latitude, lng: listing.longitude },
+        20, // radius needs to be  validated
+      ),
+    );
+  }
   return (
     <div>
-      {listings.map((listing, index) => (
+      {filteredListings.map((listing, index) => (
         <Property
+          key={listing.id}
           id={`listing-${index + 1}`}
           listing={listing}
-          key={listing.id}
         />
       ))}
     </div>
   );
 };
 
-export default ListingsContainer;
+export default memo(ListingsContainer);
