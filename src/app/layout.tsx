900a92c6b116b1b8f41a995c28ab92322848b7fb
cameochoquer <cjchoquer@gmail.com>
2023-08-01 11:39:12 +0100

    forcing dynamic cookies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,8 @@ export const metadata: Metadata = {
   title: 'rent-shield',
   description: 'rent-shield',
 }
+// export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function RootLayout({
   children,
