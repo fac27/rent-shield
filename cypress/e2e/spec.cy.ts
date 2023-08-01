f28be20262bee762c56e3aac5a0f9bf55ed46016
FomasTreeman <tom@team-freeman.com>
2023-08-01 17:24:16 +0100

    spec test fix

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,26 +1,21 @@
-describe('/ page spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000/')
-  })
-})
-
 describe('/listings page spec', () => {
   it('loads listings available', () => {
-    cy.visit('http://localhost:3000/listings')
+    cy.visit('/listings')
     cy.get('.test-class-property').should('be.visible')
   })
 })
 
 describe('/search-preferences spec', () => {
+  beforeEach(() => {
+    cy.visit('/search-preferences')
+  })
   it('loads the SearchPreferencesForm', () => {
-    cy.visit('http://localhost:3000/search-preferences')
     cy.get('form').should('be.visible')
   })
   it('submits preferences to the url', () => {
-    cy.visit('http://localhost:3000/search-preferences')
     cy.get('#location').type('hackney')
     cy.get('#max-rooms').invoke('val', 1).trigger('change')
-    cy.get('button').click({ multiple: true })
+    cy.get('[data-cy="SearchPreferencesForm"] button').click({ multiple: true })
     cy.url().should('include', '&location=hackney')
     cy.url().should('include', '&max_rooms=1')
   })
